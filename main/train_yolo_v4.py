e0ae3fc23c0d7b9ce6700c77a8dc6dfb46618d8c
irenebritney <irenebritney@192.168.31.84>
2021-03-03 00:36:40 +0800

    + os路径

--- main/train_yolo_v4.py
+++ main/train_yolo_v4.py
@@ -5,6 +5,13 @@
 @author: luoyi
 Created on 2021年3月2日
 '''
+import sys
+import os
+#    取项目根目录
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__)).split('faster_rcnn')[0]
+ROOT_PATH = ROOT_PATH + "faster_rcnn"
+sys.path.append(ROOT_PATH)
+
 import utils.conf as conf
 import utils.logger_factory as logf
 import utils.alphabet as alphabet
